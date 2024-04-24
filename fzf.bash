b111f1f29280e1390bcf9a905f195b63c37a5cb5
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-04-25 00:57:36 +0900

    adding fzf functionality for bash

--- fzf.bash
+++ fzf.bash
@@ -11,3 +11,22 @@ fi
 # Key bindings
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OTPS="--preview 'eza --tree --color=always {} | head -200'"
+
+# Advanced customization of fzf options via _fzf_comprun function 
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+
+_fzf_comprun() {
+  local command=$1 
+  shift
+
+  case "$command" in
+    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    export|unset) fzf --preview "eval 'echo \$' {}" "$@" ;;
+    ssh)          fzf --preview 'dig {}'            "$@" ;;
+    *)            fzf --preview "--preview 'bat -n --color=always --line-range :500 {}'" "$@" ;;
+  esac
+}
