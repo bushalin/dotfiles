8730e5f7fdd0f9f1303a961d468db327da0ba425
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-04-26 00:43:32 +0900

    add dashboard

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -1,17 +1,50 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "gruvbox",
+  theme = "gruvbox",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
+    modules = nil,
+  },
+
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+
+      "   ___  __  ________ _____   __   _____  __",
+      "  / _ )/ / / / __/ // / _ | / /  /  _/ |/ /",
+      " / _  / /_/ /\\ \\/ _  / __ |/ /___/ //    / ",
+      "/____/\\____/___/_//_/_/ |_/____/___/_/|_/  ",
+      "                                           ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
 }
+
 M.mappings = require("configs.dap_mappings")
 
 return M
--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -9,34 +9,29 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-map("n", "<leader>w", ":w!<cr>", { silent = true })
+map("n", "<leader>w", ":w!<cr>", { silent = true }, "Save a file")
 map("n", "<leader><cr>", ":noh<cr>", { silent = true })
-map("n", "<leader>si", ":source %<cr>", { silent = false })
+map("n", "<leader>si", ":source %<cr>", { silent = false }, "Source file")
 
-map("n", "<C-j>", "<C-W>j")
-map("n", "<C-k>", "<C-W>k")
-map("n", "<C-h>", "<C-W>h")
-map("n", "<C-l>", "<C-W>l")
 
 map("n", "dh", ":diffget //2<cr>", { silent = true })
 map("n", "dl", ":diffget //3<cr>", { silent = true })
 
--- double click jj to revert back to normal mode
--- map("i", "jj", "<ESC>", { silent = true })
 
 -- copy to the clipboard
 -- vim.api.nvim_set_option("clipboard", "unnamed")
 
 -- maximize the window
-map("n", "<C-w>m", "<C-w>|<C-w>_")
+map("n", "<C-w>m", "<C-w>|<C-w>_", "Maximize window")
 
 -- map("n", "<leader>vs", ":vs<cr><C-W>l")
 -- map("n", "<leader>sp", ":sp<cr><C-W>j")
 
-map("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>")
+map("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>", "Change current root directory")
 
-vim.cmd([[autocmd FileType c,cpp,go setlocal shiftwidth=2 softtabstop=2 expandtab]])
-vim.cmd([[autocmd FileType lua,javascript,typescript,typescriptreact setlocal shiftwidth=2 softtabstop=2 expandtab]])
+-- autocmd for common filetypes
+vim.cmd([[autocmd FileType go setlocal shiftwidth=4 softtabstop=4 expandtab]])
+vim.cmd([[autocmd FileType c, cpp, lua,javascript,typescript,typescriptreact setlocal shiftwidth=2 softtabstop=2 expandtab]])
 
 -- remaps from the primeage
 -- move selected files in visual mode
