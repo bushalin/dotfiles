a9771ec3c8acf03e47dfb57d67edf59901e50541
Md Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-19 12:26:47 +0900

    fix: formatter fix to call on save

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,15 +1,23 @@
 local options = {
+  event = { "BufReadPre", "BufNewFile" },
+
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    cpp = { "clang-format" },
+    json = { "prettier" },
+    markdown = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  },
 }
 
 require("conform").setup(options)
