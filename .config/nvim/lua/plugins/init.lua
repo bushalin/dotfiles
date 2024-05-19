c29f6a26ad47b53b8d994848268770fe9adf1279
Md Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-19 12:26:18 +0900

    Neovim: adding linters

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -63,4 +63,20 @@ return {
       -- refer to the configuration section below
     }
   },
+  {
+    "mfussenegger/nvim-lint",
+    config  = function()
+      require("configs.nvim-lint")
+
+      local lint = require("lint")
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end
+      })
+    end
+  }
 }
