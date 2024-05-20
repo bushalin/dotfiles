c658692a8781ed598429d7f91c487399873ca425
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-20 13:32:06 +0900

    neovim: formatting files

--- .config/nvim/lua/configs/dap_mappings.lua
+++ .config/nvim/lua/configs/dap_mappings.lua
@@ -1,14 +1,16 @@
 local M = {}
-  M.dap = {
-    n = {
-      ["<leader>db"] = {
-        "<cmd> DapToggleBreakpoint <CR>",
-        "Add breakpoint at line",
-      },
-      ["<leader>dr"] = {
-        "<cmd> DapContinue <CR>",
-        "Start or continue debugger",
-      },
-    }
+
+M.dap = {
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue debugger",
+    },
   }
+}
+
 return M
