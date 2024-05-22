47c04398e524884f9de6386a3267db671c21d0eb
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-22 13:37:37 +0900

    neovim: remove autocmds from mappings file

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -31,23 +31,6 @@ map("n", "<C-w>m", "<C-w>|<C-w>_", { desc = "Maximize buffer" })
 
 map("n", "<leader>cd", ":cd %:p:h<cr>:pwd<cr>", { desc = "Change root directory to current" })
 
--- autocmd for common filetypes
-vim.cmd([[autocmd FileType go setlocal shiftwidth=4 softtabstop=4 expandtab]])
-vim.cmd(
-  [[autocmd FileType c, cpp, lua,javascript,typescript,typescriptreact setlocal shiftwidth=2 softtabstop=2 expandtab]])
-
--- autocmd for db (vim-dadbod)
-vim.cmd(
-  [[autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })]])
-
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("bushalin-hightlight", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 -- remaps from the primeage
 -- move selected files in visual mode
