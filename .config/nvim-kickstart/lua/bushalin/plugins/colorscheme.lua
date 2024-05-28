8479e2542a05a1f24a8a77e8f37477b044abc671
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-28 20:55:42 +0900

    neovim: colorscheme update

--- .config/nvim-kickstart/lua/bushalin/plugins/colorscheme.lua
+++ .config/nvim-kickstart/lua/bushalin/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    enabled = true,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
@@ -16,5 +15,29 @@ return {
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+  },
+
+  {
+    {
+      'catppuccin/nvim',
+      name = 'catppuccin',
+      priority = 1000,
+      opts = {
+        flavour = 'macchiato',
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          treesitter = true,
+          gitsigns = true,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+        },
+      },
+      init = function()
+        vim.cmd.colorscheme 'catppuccin'
+      end,
+    },
   },
 }
