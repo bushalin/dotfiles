317c0e3860e9ad87dceba161ecc62f9073d364af
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-29 12:54:49 +0900

    neovim: telescope plugin source => harpoon

--- .config/nvim-kickstart/lua/bushalin/plugins/telescope.lua
+++ .config/nvim-kickstart/lua/bushalin/plugins/telescope.lua
@@ -65,6 +65,7 @@ return {
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'harpoon')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
