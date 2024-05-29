3ee5b93ca4196a278842d40980f8853e3dd4af99
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-29 13:38:36 +0900

    neovim: harpoon added keymaps

--- .config/nvim-kickstart/lua/bushalin/plugins/harpoon.lua
+++ .config/nvim-kickstart/lua/bushalin/plugins/harpoon.lua
@@ -3,12 +3,47 @@ return {
     'ThePrimeagen/harpoon',
     event = { 'BufReadPre', 'BufNewFile', 'BufEnter' },
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      'nvim-lua/plenary.nvim',
     },
     keys = {},
     config = function()
-      vim.keymap.set('n', '<leader>he', function() require('harpoon.ui').toggle_quick_menu() end, { desc = '[H]arpoon [E]xplore'})
-      vim.keymap.set('n', '<leader>ha', function() require('harpoon.mark').add_file() end, { desc = '[H]arpoon [A]dd file'})
-    end
+      local harpoon = require 'harpoon'
+      local harpoon_ui = require 'harpoon.ui'
+      local harpoon_mark = require 'harpoon.mark'
+      harpoon.setup {
+        menu = {
+          width = vim.api.nvim_win_get_width(0) - 50,
+        },
+      }
+      vim.keymap.set('n', '<leader>he', function()
+        harpoon_ui.toggle_quick_menu()
+      end, { desc = '[H]arpoon [E]xplore' })
+
+      vim.keymap.set('n', '<leader>hn', function()
+        harpoon_ui.nav_next()
+      end, { desc = '[H]arpoon navigate [N]ext' })
+      vim.keymap.set('n', '<leader>hp', function()
+        harpoon_ui.nav_prev()
+      end, { desc = '[H]arpoon navigate [N]ext' })
+      vim.keymap.set('n', '<leader>h1', function()
+        harpoon_ui.nav_file(1)
+      end, { desc = '[H]arpoon navigate file [1]' })
+      vim.keymap.set('n', '<leader>h2', function()
+        harpoon_ui.nav_file(2)
+      end, { desc = '[H]arpoon navigate file [2]' })
+      vim.keymap.set('n', '<leader>h3', function()
+        harpoon_ui.nav_file(3)
+      end, { desc = '[H]arpoon navigate file [3]' })
+      vim.keymap.set('n', '<leader>h4', function()
+        harpoon_ui.nav_file(4)
+      end, { desc = '[H]arpoon navigate file [4]' })
+      vim.keymap.set('n', '<leader>h5', function()
+        harpoon_ui.nav_file(5)
+      end, { desc = '[H]arpoon navigate file [5]' })
+
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon_mark.add_file()
+      end, { desc = '[H]arpoon [A]dd file' })
+    end,
   },
 }
