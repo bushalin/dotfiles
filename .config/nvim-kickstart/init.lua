290cdc1247d9dde76d94bc081606dd8ad68a8186
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-29 12:53:43 +0900

    neovim: lazy config change

--- .config/nvim-kickstart/init.lua
+++ .config/nvim-kickstart/init.lua
@@ -18,7 +18,18 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+local lazy_settings = {
+  defaults = {
+    lazy = true,
+    install = {
+      colorscheme = { 'gruvbox' },
+    },
+  },
+}
+
 -- NOTE: Here is where you install your plugins.
+local lazyconfig = require 'bushalin.lazy_config'
+
 require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
@@ -30,33 +41,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   --
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'bushalin.plugins' },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
+}, lazyconfig)
 
 vim.schedule(function()
   require 'bushalin.mappings'
