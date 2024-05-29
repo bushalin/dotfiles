25cb3b799499541f3648978fd23b4e74be5fb0b2
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-05-29 13:39:26 +0900

    neovim: fix telescope keymaps

--- .config/nvim-kickstart/lua/bushalin/plugins/telescope.lua
+++ .config/nvim-kickstart/lua/bushalin/plugins/telescope.lua
@@ -74,7 +74,7 @@ return {
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>flg', builtin.live_grep, { desc = '[F]ind [L]ive [G]rep' })
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = '[F]ind [M]arks' })
