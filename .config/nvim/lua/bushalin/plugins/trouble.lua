09cf359cca830a43a84c9eea1c959fc4d5eab8ab
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-02 18:34:41 +0900

    neovim: adding new trouble config

--- .config/nvim/lua/bushalin/plugins/trouble.lua
+++ .config/nvim/lua/bushalin/plugins/trouble.lua
@@ -1,12 +1,39 @@
 return {
   {
     'folke/trouble.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = { '<leader>tt', '<leader>tw','<leader>td', '<leader>tq', 'gR' },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>td',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = '[T]rouble [D]iagnostics toggle',
+      },
+      {
+        '<leader>tD',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = '[T]rouble buffer [D]iagnostics',
+      },
+      {
+        '<leader>ts',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = '[T]rouble [S]ymbols',
+      },
+      {
+        '<leader>tl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = '[T]rouble [L]SP diagnostics / references etc',
+      },
+      {
+        '<leader>tL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = '[T]rouble [L]ocation List',
+      },
+      {
+        '<leader>tq',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = '[T]rouble [Q]uickfist List',
+      },
     },
     config = function()
       vim.keymap.set('n', '<leader>tt', function()
