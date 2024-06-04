1ab997fdb2e4b928afb874156e1b6276e673e417
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-05 01:17:12 +0900

    neovim: colorscheme remove comments

--- .config/nvim/lua/bushalin/plugins/colorscheme.lua
+++ .config/nvim/lua/bushalin/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
+local util = require 'telescope.pickers.c'
 return {
   { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     opts = {
       style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      light_style = 'day', -- The theme is used when the background is set to light
       transparent = false, -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
       styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
@@ -21,20 +16,13 @@ return {
         sidebars = 'dark', -- style for sidebars, see below
         floats = 'dark', -- style for floating windows
       },
-      sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
       hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
       dim_inactive = false, -- dims inactive windows
       lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
     },
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
 
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -43,7 +31,6 @@ return {
     {
       'catppuccin/nvim',
       name = 'catppuccin',
-      priority = 1000,
       opts = {
         flavour = 'macchiato', -- latte, frappe, macchiato, mocha
         background = { -- :h background
@@ -51,7 +38,6 @@ return {
           dark = 'mocha',
         },
         transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
           enabled = false, -- dims the background color of inactive window
@@ -76,8 +62,6 @@ return {
           operators = {},
           -- miscs = {}, -- Uncomment to turn off hard-coded styles
         },
-        color_overrides = {},
-        custom_highlights = {},
         default_integrations = true,
         integrations = {
           cmp = true,
@@ -89,12 +73,8 @@ return {
             enabled = true,
             indentscope_color = '',
           },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       },
-      init = function()
-        vim.cmd.colorscheme 'catppuccin'
-      end,
     },
   },
 
@@ -125,5 +105,9 @@ return {
       dim_inactive = false,
       transparent_mode = false,
     },
+  },
+
+  {
+    'savq/melange-nvim',
   },
 }
