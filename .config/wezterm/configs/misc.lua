76173d52cd53714750d4f4ee23a26c68328e4e21
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-10 17:50:28 +0900

    wezterm: native macos fullscreen mode

--- .config/wezterm/configs/misc.lua
+++ .config/wezterm/configs/misc.lua
@@ -3,6 +3,7 @@ local module = {}
 function module.apply(config)
   config.send_composed_key_when_left_alt_is_pressed = false
   config.send_composed_key_when_right_alt_is_pressed = true
+  config.native_macos_fullscreen_mode = true
 end
 
 return module
