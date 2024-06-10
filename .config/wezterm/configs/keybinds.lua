761e22312688cab53469a93e30b446fb319d0c12
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-10 17:49:23 +0900

    wezterm: adding misc function

--- .config/wezterm/configs/keybinds.lua
+++ .config/wezterm/configs/keybinds.lua
@@ -13,6 +13,26 @@ wezterm.on('toggle-opacity', function(window, pane)
   end
   window:set_config_overrides(overrides)
 end)
+
+wezterm.on('open-nvim-with-scrollback', function(window, pane)
+  local scrollback_text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+  local filename = os.tmpname()
+  local f = io.open(filename, 'w+')
+  f:write(scrollback_text)
+  f:flush()
+  f:close()
+
+  local editor_command = { 'zsh', '--login', '-c', 'nvim ' .. filename }
+  local _, new_pane, _ = window:mux_window():spawn_tab { args = editor_command }
+  new_pane:send_text 'ge'
+
+  -- Wait "enough" time for the editor to read the file before removing it.
+  -- (Reading the file is asynchronous and not awaitable.)
+  wezterm.sleep_ms(1000)
+  os.remove(filename)
+end)
+
 function keybinds.apply(config)
   -- config.leader = { key = 'w', mods = 'CTRL|SHIFT', timeout_milliseconds = 1000 }
   config.keys = {
@@ -21,6 +41,11 @@ function keybinds.apply(config)
       mods = 'CTRL|SHIFT',
       action = wezterm.action.EmitEvent 'toggle-opacity',
     },
+    { key = 'o', mods = 'ALT', action = wezterm.action.EmitEvent 'open-nvim-with-scrollback' },
+    { key = 'h', mods = 'CMD', action = act.ActivatePaneDirection 'Left' },
+    { key = 'l', mods = 'CMD', action = act.ActivatePaneDirection 'Right' },
+    { key = 'j', mods = 'CMD', action = act.ActivatePaneDirection 'Down' },
+    { key = 'k', mods = 'CMD', action = act.ActivatePaneDirection 'Up' },
   }
 end
 
