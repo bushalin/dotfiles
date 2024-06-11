8c0e67592a456165d477fb0d38f97daf40cda834
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-11 16:54:48 +0900

    neovim: support for dap virtual text

--- .config/nvim/lua/bushalin/plugins/debug.lua
+++ .config/nvim/lua/bushalin/plugins/debug.lua
@@ -48,6 +48,9 @@ return {
       },
     }
 
+    -- setup virtual text for nvim-dap
+    require('nvim-dap-virtual-text').setup()
+
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
