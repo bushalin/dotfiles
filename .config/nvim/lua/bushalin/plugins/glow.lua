b9bf6dd0de82cb1982b89b48fcb701e072df99aa
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-14 16:34:08 +0900

    neovim: markdown extentions

---
+++ .config/nvim/lua/bushalin/plugins/glow.lua
@@ -0,0 +1,8 @@
+return {
+  -- plugin to preview .md files in neovim
+  {
+    'ellisonleao/glow.nvim',
+    config = true,
+    cmd = 'Glow',
+  },
+}
---
+++ .config/nvim/lua/bushalin/plugins/vimwiki.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'vimwiki/vimwiki',
+    ft = '.md',
+    event = 'BufEnter *.md',
+    keys = { '<leader>ww', '<leader>wt', '<leader>ws' },
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          path = '~/Documents/vimwiki/',
+          syntax = 'markdown',
+          ext = '.md',
+        },
+        {
+          path = '~/Documents/Work/Notes/',
+          syntax = 'markdown',
+          ext = '.md',
+        }
+      }
+      vim.g.vimwiki_ext2syntax = {
+        ['.md'] = 'markdown',
+        ['.markdown'] = 'markdown',
+        ['.mdown'] = 'markdown',
+      }
+    end,
+  },
+}
