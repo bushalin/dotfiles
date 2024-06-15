d519f7ef86d962b14d0e7e2f58d314b5b46abaad
Md Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-16 03:01:45 +0900

    neovim: adding for updating to 0.10.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ' '
 
+vim.g.notermguicolors = false
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
