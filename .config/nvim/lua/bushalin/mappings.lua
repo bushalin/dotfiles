9ac795a8bcfbe74dcdbe1c71805edfde13904e27
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-16 14:49:58 +0900

    neovim: keymap disable for nvim >= 0.10.0

--- .config/nvim/lua/bushalin/mappings.lua
+++ .config/nvim/lua/bushalin/mappings.lua
@@ -6,8 +6,8 @@ map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 map('n', ';', ':')
 
 -- Diagnostic keymaps
-map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
