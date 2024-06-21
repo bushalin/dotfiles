02e2389f9d4dac4c68fc6351667e959694e4def2
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-06-21 17:22:49 +0900

    neovim: remove comments from init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,16 +24,6 @@ vim.opt.rtp:prepend(lazypath)
 local lazyconfig = require 'bushalin.configs.lazy_config'
 
 require('lazy').setup({
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
   -- initialize all the core plugins that we absolutely need
   { import = 'bushalin.plugins.core' },
 
