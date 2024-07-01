43b6fe29328b2e1bd2c077d0879f366985402c66
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-07-01 14:36:53 +0900

    neovim: obsidian settings adjust

--- .config/nvim/lua/bushalin/plugins/addon/obsidian.lua
+++ .config/nvim/lua/bushalin/plugins/addon/obsidian.lua
@@ -5,7 +5,7 @@ return {
     lazy = true,
     ft = '.md',
     event = 'BufEnter *.md',
-    keys = { '<leader>oo', '<leader>on', '<leader>od'},
+    keys = { '<leader>oo', '<leader>on', '<leader>od' },
     dependencies = {
       -- Required.
       'nvim-lua/plenary.nvim',
@@ -50,7 +50,7 @@ return {
           -- Optional, if you want to change the date format of the default alias of daily notes.
           alias_format = '%B %-d, %Y',
           -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-          template = nil,
+          template = 'daily_note.md',
         },
 
         -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
@@ -180,7 +180,11 @@ return {
           date_format = '%Y-%m-%d',
           time_format = '%H:%M',
           -- A map for custom variables, the key should be the variable and the value a function
-          substitutions = {},
+          substitutions = {
+            author = function()
+              return 'Md. Hasibul Hasan'
+            end,
+          },
         },
 
         -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
@@ -195,14 +199,61 @@ return {
         -- Optional, set to true if you use the Obsidian Advanced URI plugin.
         -- https://github.com/Vinzent03/obsidian-advanced-uri
         use_advanced_uri = false,
+
+        -- Optional, configure additional syntax highlighting / extmarks.
+        -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+        ui = {
+          enable = true, -- set to false to disable all additional syntax features
+          update_debounce = 200, -- update delay after a text change (in milliseconds)
+          max_file_length = 5000, -- disable UI features for files with more than this many lines
+          -- Define how various check-boxes are displayed
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+            [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+            ['x'] = { char = '', hl_group = 'ObsidianDone' },
+            ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+            ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+            ['!'] = { char = '', hl_group = 'ObsidianImportant' },
+            -- Replace the above with this if you don't have a patched font:
+            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+            -- You can also add more custom ones...
+          },
+          -- Use bullet marks for non-checkbox lists.
+          bullets = { char = '•', hl_group = 'ObsidianBullet' },
+          external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+          -- Replace the above with this if you don't have a patched font:
+          -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          reference_text = { hl_group = 'ObsidianRefText' },
+          highlight_text = { hl_group = 'ObsidianHighlightText' },
+          tags = { hl_group = 'ObsidianTag' },
+          block_ids = { hl_group = 'ObsidianBlockID' },
+          hl_groups = {
+            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+            ObsidianTodo = { bold = true, fg = '#f78c6c' },
+            ObsidianDone = { bold = true, fg = '#89ddff' },
+            ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+            ObsidianTilde = { bold = true, fg = '#ff5370' },
+            ObsidianImportant = { bold = true, fg = '#d73128' },
+            ObsidianBullet = { bold = true, fg = '#89ddff' },
+            ObsidianRefText = { underline = true, fg = '#c792ea' },
+            ObsidianExtLinkIcon = { fg = '#c792ea' },
+            ObsidianTag = { italic = true, fg = '#89ddff' },
+            ObsidianBlockID = { italic = true, fg = '#89ddff' },
+            ObsidianHighlightText = { bg = '#75662e' },
+          },
+        },
       }
       obsidian.setup(opts)
 
-      vim.keymap.set('n', '<leader>oo',function ()
-        local telescope = require('telescope.builtin')
+      -- NOTE:
+      -- Set custom keymaps
+      vim.keymap.set('n', '<leader>oo', function()
+        local telescope = require 'telescope.builtin'
         local notes_path = os.getenv 'HOME' .. '/Documents/obsidian/personal/'
         telescope.find_files { cwd = notes_path }
-      end, {desc = '[O]bsidian [O]pen'})
+      end, { desc = '[O]bsidian [O]pen' })
 
       vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<cr>', { desc = '[O]bsidian [N]ew' })
       vim.keymap.set('n', '<leader>od', '<cmd>ObsidianToday<cr>', { desc = '[O]bsidian [D]aily TODAY' })
