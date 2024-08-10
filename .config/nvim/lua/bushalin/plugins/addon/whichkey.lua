b8241ecc9b1e0cd583679f79e9ac34851104af17
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-08-10 14:23:56 +0900

    update: whichkey update configuration

--- .config/nvim/lua/bushalin/plugins/addon/whichkey.lua
+++ .config/nvim/lua/bushalin/plugins/addon/whichkey.lua
@@ -4,35 +4,39 @@ return {
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore', c = { 'Codesnap [C]opy'}, s = { 'Codesnap [S]ave'} },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore', l = { '[L]ive [G]rep' }, g = { '[G]it' }, t = { '[T]hemes'} },
-        ['<leader>w'] = { name = '[W]orkspace/[W]iki', _ = 'which_key_ignore' },
-        ['<leader>o'] = {
-          name = '[O]bsidian',
-          _ = 'which_key_ignore',
-          n = {'[O]bsidian [N]ew'},
-          t = { '[O]bsidian [T]emplate'},
-          d = { '[O]bsidian [D]aily'}
-        },
-        ['<leader>t'] = { name = '[T]rouble', _ = 'which_key_ignore' },
-        -- NOTE:
-        -- you can specify second level of prefixes like this
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore', t = { '[T]oggle' } },
-        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-        ['<leader>u'] = { name = '[U]ndo Tree', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]ource', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>cc', desc = 'Codesnap [C]opy' },
+        { '<leader>cs', desc = 'Codesnap [S]ave' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>f', group = '[F]ind' },
+        { '<leader>f_', hidden = true },
+        { '<leader>fg', desc = '[G]it' },
+        { '<leader>fl', desc = '[L]ive [G]rep' },
+        { '<leader>ft', desc = '[T]hemes' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>g_', hidden = true },
+        { '<leader>gt', desc = '[T]oggle' },
+        { '<leader>h', group = '[H]arpoon' },
+        { '<leader>h_', hidden = true },
+        { '<leader>o', group = '[O]bsidian' },
+        { '<leader>o_', hidden = true },
+        { '<leader>od', desc = '[O]bsidian [D]aily' },
+        { '<leader>on', desc = '[O]bsidian [N]ew' },
+        { '<leader>ot', desc = '[O]bsidian [T]emplate' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]ource' },
+        { '<leader>s_', hidden = true },
+        { '<leader>t', group = '[T]rouble' },
+        { '<leader>t_', hidden = true },
+        { '<leader>u', group = '[U]ndo Tree' },
+        { '<leader>u_', hidden = true },
+        { '<leader>w', group = '[W]orkspace/[W]iki' },
+        { '<leader>w_', hidden = true },
       }
-      -- WARN:
-      -- visual mode throws error in the updated version
-      --
-      -- require('which-key').register({
-      --   ['<leader>g'] = { '[G]it' },
-      -- }, { mode = 'v' })
     end,
   },
 }
