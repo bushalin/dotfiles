a76acf09dd3277f6a7177c07b5a6c38f163e61df
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-08-18 03:43:30 +0900

    fix: cleanup wezterm config

--- .config/wezterm/configs/background.lua
+++ .config/wezterm/configs/background.lua
@@ -2,26 +2,6 @@ local wezterm = require 'wezterm'
 local background = {}
 
 function background.apply(config)
-  -- config.window_background_opacity = 1.0
-  -- config.text_background_opacity = 0.7
-  --
-  -- -- path to the file must be a full path instead of relative path
-  -- --
-  -- config.window_background_image = wezterm.config_dir .. '/assets/katana_wallpaper.jpeg'
-  -- config.window_background_image = wezterm.config_dir .. '/assets/cyberpunk-edgerunners-lucy-game-4k.jpeg'
-  --
-  -- config.window_background_image_hsb = {
-  --   -- Darken the background image by reducing it to 1/3rd
-  --   brightness = 0.08,
-  --
-  --   -- You can adjust the hue by scaling its value.
-  --   -- a multiplier of 1.0 leaves the value unchanged.
-  --   hue = 1.0,
-  --
-  --   -- You can adjust the saturation also.
-  --   saturation = 1.0,
-  -- }
-
   config.text_background_opacity = 0.85
   config.background = {
     {
