b092e9bd2f95622eb2427f0afb92c974a18823e9
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-08-20 23:45:45 +0900

    fix: fzf preview

--- .fzf.zsh
+++ .fzf.zsh
@@ -45,7 +45,8 @@ _fzf_comprun() {
   shift
 
   case "$command" in
-    cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+    cd)           fzf --preview 'eza --tree -L 2 --color=always {} | head -200' "$@" ;;
+    ls)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
     export|unset) fzf --preview "eval 'echo \$' {}" "$@" ;;
     ssh)          fzf --preview 'dig {}'            "$@" ;;
     *)            fzf --preview "bat -n --color=always --line-range :500 {}" "$@" ;;
