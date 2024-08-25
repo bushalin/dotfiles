19d63f9fd741ba4d7278d624135bd59cafc0a18b
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-08-25 21:59:12 +0900

    mini: disabling startup screen

--- .config/nvim/lua/bushalin/plugins/core/mini.lua
+++ .config/nvim/lua/bushalin/plugins/core/mini.lua
@@ -39,28 +39,28 @@ return {
 
       -- NOTE:
       -- Startup screen
-      local starter = require 'mini.starter'
-      starter.setup {
-        evaluate_single = true,
-        header = [[
-         ___  __  ________ _____   __   _____  __
-        / _ )/ / / / __/ // / _ | / /  /  _/ |/ /
-       / _  / /_/ /\ \/ _  / __ |/ /___/ //    /
-      /____/\____/___/_//_/_/ |_/____/___/_/|_/
-              ]],
-        items = {
-          starter.sections.builtin_actions(),
-          starter.sections.recent_files(10, false),
-          starter.sections.recent_files(5, true),
-          starter.sections.telescope(),
-        },
-        content_hooks = {
-          starter.gen_hook.adding_bullet(),
-          starter.gen_hook.indexing('all', { 'Builtin actions' }),
-          starter.gen_hook.aligning('center', 'center'),
-          starter.gen_hook.padding(3, 2),
-        },
-      }
+      -- local starter = require 'mini.starter'
+      -- starter.setup {
+      --   evaluate_single = true,
+      --   header = [[
+      --    ___  __  ________ _____   __   _____  __
+      --   / _ )/ / / / __/ // / _ | / /  /  _/ |/ /
+      --  / _  / /_/ /\ \/ _  / __ |/ /___/ //    /
+      -- /____/\____/___/_//_/_/ |_/____/___/_/|_/
+      --         ]],
+      --   items = {
+      --     starter.sections.builtin_actions(),
+      --     starter.sections.recent_files(10, false),
+      --     starter.sections.recent_files(5, true),
+      --     starter.sections.telescope(),
+      --   },
+      --   content_hooks = {
+      --     starter.gen_hook.adding_bullet(),
+      --     starter.gen_hook.indexing('all', { 'Builtin actions' }),
+      --     starter.gen_hook.aligning('center', 'center'),
+      --     starter.gen_hook.padding(3, 2),
+      --   },
+      -- }
     end,
   },
 }
