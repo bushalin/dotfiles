6b0ff3d10b3e5cca3b9e02b9bfb1a92641705284
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-08-26 17:15:16 +0900

    add: plugins added for toggling terminals

---
+++ .config/nvim/lua/bushalin/plugins/addon/terminal.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    -- amongst your other plugins
+    {'akinsho/toggleterm.nvim', version = "*", config = true}
+  },
+  {
+    "ryanmsnyder/toggleterm-manager.nvim",
+    dependencies = {
+      "akinsho/nvim-toggleterm.lua",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
+    },
+    config = true,
+  }
+}
