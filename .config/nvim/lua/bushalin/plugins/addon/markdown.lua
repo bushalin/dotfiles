50540667e898c931eed3580df2a4c9ec375cf287
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-10-08 18:07:52 +0900

    feat: add markdown plugins to Neovim configuration
    - Add kiwi.nvim for managing markdown wikis
    - Add headlines.nvim for better markdown headlines
    - Add bullets.vim for improved bullet points in markdown
    - Add markdown-preview.nvim for live markdown preview
    - Add outline.nvim for markdown document outline

---
+++ .config/nvim/lua/bushalin/plugins/addon/markdown.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    'serenevoid/kiwi.nvim',
+    ft = '.md',
+    event = 'BufEnter *.md',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      {
+        name = 'work',
+        path = '/Users/hasibulhasan/Documents/Work/Projects',
+      },
+      {
+        name = 'personal',
+        path = '/Users/hasibulhasan/Documents/vimwiki',
+      },
+    },
+    keys = {
+      { '<localleader>ww', ':lua require("kiwi").open_wiki_index()<cr>', desc = 'Open Wiki index' },
+      { '<localleader>wp', ':lua require("kiwi").open_wiki_index("personal")<cr>', desc = 'Open index of personal wiki' },
+      { 'T', ':lua require("kiwi").todo.toggle()<cr>', desc = 'Toggle Markdown Task' },
+    },
+    lazy = true,
+  },
+
+  {
+    'lukas-reineke/headlines.nvim',
+    enabled = false,
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    config = true, -- or `opts = {}`
+  },
+
+  {
+    -- Better bullet points for markdown
+    'bullets-vim/bullets.vim',
+    ft = 'markdown',
+    event = 'BufEnter *.md',
+  },
+
+  -- install with yarn or npm
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+
+  {
+    'hedyhli/outline.nvim',
+    ft = '.md',
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+      require('outline').setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
+}
