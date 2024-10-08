3159b40de985a14a75fa40e93035033c3343cfab
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-10-08 18:04:15 +0900

    feat: add key mappings for Notify plugin in Neovim
    - Add key mapping to dismiss notifications in normal mode
    - Add key mapping to dismiss notifications in insert mode
    - Add key mapping to open notifications in Telescope
    - Update notify.lua to include these key mappings

--- .config/nvim/lua/bushalin/plugins/addon/notify.lua
+++ .config/nvim/lua/bushalin/plugins/addon/notify.lua
@@ -22,5 +22,13 @@ return {
       timeout = 5000,
       top_down = true,
     },
+    config = function(_, opts)
+      require('notify').setup(opts)
+      -- Dismiss notifications
+      vim.keymap.set('n', '<localleader>nn', '<cmd>lua require("notify").dismiss()<CR>', { noremap = true, silent = true, desc = '[N]otify [D]ismiss' })
+      vim.keymap.set('i', '<C-n>', '<cmd>lua require("notify").dismiss()<CR>', { noremap = true, silent = true, desc = '[N]otify [D]ismiss' })
+      -- Open notifications in telescope
+      vim.keymap.set('n', '<localleader>no', '<cmd>Telescope notify<CR>', { silent = true, desc = '[N]otify [O]pen in telescope' })
+    end,
   },
 }
