221e47350e865039d04c2d28de201252a2b0efdc
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-10-08 18:15:08 +0900

    feat: update Codeium plugin and configuration
    - Changed plugin from 'Exafunction/codeium.vim' to 'Exafunction/codeium.nvim'
    - Updated dependencies to include 'nvim-lua/plenary.nvim' and 'hrsh7th/nvim-cmp'
    - Enabled plugin for 'BufRead' and 'BufNewFile' events
    - Added configuration to enable chat feature in Codeium

--- .config/nvim/lua/bushalin/plugins/addon/codeium.lua
+++ .config/nvim/lua/bushalin/plugins/addon/codeium.lua
@@ -1,20 +1,15 @@
 return {
   {
-    'Exafunction/codeium.vim',
-    enabled = false,
+    'Exafunction/codeium.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'hrsh7th/nvim-cmp',
     },
     event = { 'BufRead', 'BufNewFile' },
-    -- config = function()
-    --   vim.keymap.set('i', '<C-g>', function()
-    --     return vim.fn['codeium#Accept']
-    --   end, { expr = true, silent = true })
-    --   -- set a keymap to cycle through codeium suggestions
-    --   vim.keymap.set('i', '<C-,>', function()
-    --     return vim.fn['codeium#Complete()']
-    --   end, { expr = true, silent = true })
-    -- end,
+    config = function()
+      require('codeium').setup({
+        enable_chat = true
+      });
+    end,
   },
 }
