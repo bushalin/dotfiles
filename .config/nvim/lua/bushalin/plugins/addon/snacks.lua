511f2e8f57c0a86d48881f64847999d6c73d692a
Hasibul Hasan <hasibulhasan2009@gmail.com>
2024-12-17 10:51:30 +0900

    dev: update snacks plugin config

--- .config/nvim/lua/bushalin/plugins/addon/snacks.lua
+++ .config/nvim/lua/bushalin/plugins/addon/snacks.lua
@@ -40,22 +40,36 @@ return {
         },
       },
       notifier = {
-        enabled = true,
-        timeout = 3000,
-        style = {
-          {
-            border = 'rounded',
-            zindex = 100,
-            ft = 'markdown',
-            wo = {
-              winblend = 5,
-              wrap = false,
-              conceallevel = 2,
-              colorcolumn = '',
-            },
-            bo = { filetype = 'snacks_notif' },
-          },
+        timeout = 3000, -- default timeout in ms
+        width = { min = 40, max = 0.4 },
+        height = { min = 1, max = 0.6 },
+        -- editor margin to keep free. tabline and statusline are taken into account automatically
+        margin = { top = 0, right = 1, bottom = 0 },
+        padding = true, -- add 1 cell of left/right padding to the notification window
+        sort = { 'level', 'added' }, -- sort by level and time
+        -- minimum log level to display. TRACE is the lowest
+        -- all notifications are stored in history
+        level = vim.log.levels.TRACE,
+        icons = {
+          error = ' ',
+          warn = ' ',
+          info = ' ',
+          debug = ' ',
+          trace = ' ',
         },
+        keep = function(notif)
+          return vim.fn.getcmdpos() > 0
+        end,
+        ---@type snacks.notifier.style
+        style = 'compact',
+        top_down = true, -- place notifications from top to bottom
+        date_format = '%R', -- time format for notifications
+        -- format for footer when more lines are available
+        -- `%d` is replaced with the number of lines.
+        -- only works for styles with a border
+        ---@type string|boolean
+        more_format = ' ↓ %d lines ',
+        refresh = 50, -- refresh at most every 50ms
       },
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
@@ -65,8 +79,22 @@ return {
           wo = { wrap = true }, -- Wrap notifications
         },
       },
+      zen = { enabled = true },
+      toggle = {
+        which_key = true,
+        notify = true,
+      },
+      animate = { enabled = true },
+      input = { enabled = true },
     },
     keys = {
+      {
+        '<leader>z',
+        function()
+          Snacks.zen()
+        end,
+        desc = 'Toggle Zen'
+      },
       {
         '<leader>.',
         function()
